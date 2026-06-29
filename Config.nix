d71f2ea207abee1ea2eeafcd14783388e834004b
Yutsuna <leo.quinzler@epitech.eu>
2026-06-29 23:29:51 +0200

    Chore(Config): Add my Config as Example

---
+++ Config.nix
@@ -0,0 +1,19 @@
+{
+  user = {
+    name = "Yutsuna";
+    email = "leo.quinzler@epitech.eu";
+    home = "/home/Yutsuna";
+  };
+
+  system = {
+    hostname = "NixOS";
+    timezone = "Europe/Paris";
+    locale = "en_US.UTF-8";
+    keyMap = "fr";
+  };
+
+  hardware = {
+    rootUUID = "cec5ba6d-ed28-40b6-bf78-55fc36391413";
+    bootUUID = "C2FD-FF2F";
+  };
+}
