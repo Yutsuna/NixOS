a5977052f3958d886907144b0c752befe2a690a0
Yutsuna <leo.quinzler@epitech.eu>
2026-06-30 21:35:07 +0200

    Fix(Browser): Remove default extension

--- Home/Browser/Firefox/default.nix
+++ Home/Browser/Firefox/default.nix
@@ -14,18 +14,6 @@
       DisableFirefoxAccounts = true;
       OfferToSaveLogins = false;
       PasswordManagerEnabled = false;
-
-      "3rdparty".Extensions = {
-        "uBlock0@raymondhill.net" = {
-          adminSettings = {
-            userSettings = {
-              uiTheme = "light";
-              uiAccentCustom = true;
-              uiAccentCustom0 = "#CA9EE6";
-            };
-          };
-        };
-      };
     };
 
     profiles.default = {
