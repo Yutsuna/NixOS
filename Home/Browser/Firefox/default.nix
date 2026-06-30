58d4e8785c40bb91ce518ca0aba1c352d22ace0d
Yutsuna <leo.quinzler@epitech.eu>
2026-06-30 21:27:55 +0200

    Chore(Browser): Add Firefox

---
+++ Home/Browser/Firefox/default.nix
@@ -0,0 +1,42 @@
+{
+  pkgs,
+  ...
+}:
+{
+  programs.firefox = {
+    enable = true;
+
+    package = pkgs.firefox;
+
+    policies = {
+      DisableTelemetry = true;
+      DisablePocket = true;
+      DisableFirefoxAccounts = true;
+      OfferToSaveLogins = false;
+      PasswordManagerEnabled = false;
+
+      "3rdparty".Extensions = {
+        "uBlock0@raymondhill.net" = {
+          adminSettings = {
+            userSettings = {
+              uiTheme = "light";
+              uiAccentCustom = true;
+              uiAccentCustom0 = "#CA9EE6";
+            };
+          };
+        };
+      };
+    };
+
+    profiles.default = {
+      id = 0;
+      name = "default";
+      isDefault = true;
+
+      search = {
+        force = true;
+        default = "Google";
+      };
+    };
+  };
+}
