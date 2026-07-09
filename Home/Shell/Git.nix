c29d4f256425c2895222dfd2495f140fcd636858
Yutsuna <leo.quinzler@epitech.eu>
2026-07-09 22:53:13 +0200

    Fix(Git): Now runs as a job

--- Home/Shell/Git.nix
+++ Home/Shell/Git.nix
@@ -11,7 +11,7 @@
     };
     aliases = {
       cleanup = "!git fetch --prune && git branch -vv | awk '/: gone]/ {print $1}' | xargs -r git branch -d";
-      open = "!git config --get remote.origin.url | sed -E -e 's/git@([^:]+):/https:\\/\\/\\1\\//' -e 's/\\.git$//' | xargs xdg-open 2>/dev/null || git config --get remote.origin.url | sed -E -e 's/git@([^:]+):/https:\\/\\/\\1\\//' -e 's/\\.git$//' | xargs open";
+      open = "!git config --get remote.origin.url | sed -E -e 's/git@([^:]+):/https:\\/\\/\\1\\//' -e 's/\\.git$//' | xargs xdg-open 2>/dev/null || git config --get remote.origin.url | sed -E -e 's/git@([^:]+):/https:\\/\\/\\1\\//' -e 's/\\.git$//' | xargs open&";
     };
   };
 }
