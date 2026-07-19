b46665f82c7985e0699012824284ee3e81fd6337
Yutsuna <leo.quinzler@epitech.eu>
2026-07-19 19:46:48 +0200

    Chore(System): Add bluetooth

---
+++ System/Bluetooth.nix
@@ -0,0 +1,26 @@
+{
+  ...
+}:
+
+{
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+    settings = {
+      General = {
+        Experimental = true;
+        FastConnectable = true;
+      };
+    };
+  };
+
+  services.blueman.enable = true;
+
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+}
