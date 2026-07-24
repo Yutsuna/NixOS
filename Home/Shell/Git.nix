c7bb148b5a89b20ccdfd81c9011d98b926ef98df
Yutsuna <leo.quinzler@epitech.eu>
2026-07-24 12:39:53 +0200

    Style(Home): Rename git.aliases -> git.settings.aliases

--- Home/Shell/Git.nix
+++ Home/Shell/Git.nix
@@ -8,10 +8,10 @@
         name = vars.user.name;
         email = vars.user.email;
       };
-    };
-    aliases = {
-      cleanup = "!git fetch --prune && git branch -vv | awk '/: gone]/ {print $1}' | xargs -r git branch -d";
-      open = "!git config --get remote.origin.url | sed -E -e 's/git@([^:]+):/https:\\/\\/\\1\\//' -e 's/\\.git$//' | xargs xdg-open 2>/dev/null || git config --get remote.origin.url | sed -E -e 's/git@([^:]+):/https:\\/\\/\\1\\//' -e 's/\\.git$//' | xargs open&";
+      aliases = {
+        cleanup = "!git fetch --prune && git branch -vv | awk '/: gone]/ {print $1}' | xargs -r git branch -d";
+        open = "!git config --get remote.origin.url | sed -E -e 's/git@([^:]+):/https:\\/\\/\\1\\//' -e 's/\\.git$//' | xargs xdg-open 2>/dev/null || git config --get remote.origin.url | sed -E -e 's/git@([^:]+):/https:\\/\\/\\1\\//' -e 's/\\.git$//' | xargs open&";
+      };
     };
   };
 }
