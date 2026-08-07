d9993452697926f4d8e059d0ef5b591adc817e0a
Yutsuna <leo.quinzler@epitech.eu>
2026-08-07 15:20:33 +0200

    Refactor(Hyprland): Refactor  Window rules declarative

--- Home/Hyprland/Lua/windowrules.lua
+++ Home/Hyprland/Lua/windowrules.lua
@@ -1,7 +1,18 @@
-hl.window_rule({ match = { class = "^(kitty)$" }, opacity = "0.80 0.80 1.0" })
-hl.window_rule({ match = { class = "^(firefox)$" }, opacity = "0.90 0.90 1.0" })
-hl.window_rule({ match = { class = "^(zeditor)$" }, opacity = "0.80 0.80 1.0" })
-hl.window_rule({ match = { class = "^(jetbrains-clion)$" }, opacity = "0.80 0.80 1.0" })
+local opacity_window_rule = {
+    { "kitty",           "0.80 0.80 1.0" },
+    { "firefox",         "0.90 0.90 1.0" },
+    { "zeditor",         "0.80 0.80 1.0" },
+    { "jetbrains-clion", "0.80 0.80 1.0" },
+    { "vesktop",         "0.80 0.80 1.0" },
+}
+
+for _, rule in ipairs(opacity_window_rule) do
+    local class, opacity = rule[1], rule[2]
+    hl.window_rule({
+        match = { class = "^(" .. class .. ")$" },
+        opacity = opacity
+    })
+end
 
 
 local blurred_layers = {
